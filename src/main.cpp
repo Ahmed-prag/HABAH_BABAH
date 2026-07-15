859cf85ae7917f6be00ce7d0781c4a79034f677d
Ahmed M <ahmedwirba0@gmail.com>
2026-07-15 06:41:28 +0600

    further updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,49 @@
 #include "raylib.h"
-
+//
 int main() {
-    int ballX = 400;
-    int ballY = 400;
-    int radius = 20;
-    int windowX = 1350, windowY = 800;
-
+    int ballX=400;
+    int ballY=400;
+    int radius=20;
+    int windowX=1350, windowY=800;
     // Vitesse de la balle
-    int dx = 5;
-    int dy = 5;
-
+    int dx=5;
+    int dy=5;
     // Définition des marges et dimensions du terrain
-    int fieldMarginX = 50;
-    int fieldMarginY = 50;
-    int fieldWidth = windowX - (fieldMarginX * 2);
-    int fieldHeight = windowY - (fieldMarginY * 2);
-
+    int fieldMarginX=50;
+    int fieldMarginY=50;
+    int fieldWidth=windowX-(fieldMarginX*2); // fois deux à cause des deux marges
+    int fieldHeight=windowY-(fieldMarginY*2); // meme chose
     // Dimensions des cages (goals)
-    int goalWidth = 30;
-    int goalHeight = 180;
-    int goalY = fieldMarginY + (fieldHeight / 2) - (goalHeight / 2);
+    int goalWidth=30;
+    int goalHeight=180;
+    int goalY=fieldMarginY+(fieldHeight/2)-(goalHeight/2);
 
-    InitWindow(windowX, windowY, "Bouncing Ball - Terrain Complet");
+    InitWindow(windowX, windowY, "Ball-Terrain Complet");
     SetTargetFPS(60);
-
     while (!WindowShouldClose()) {
-        // Mise à jour de la position
-        ballX += dx;
-        ballY += dy;
-
         // Rebond sur les bords de la FENÊTRE
-        if (ballX < radius || ballX > windowX - radius) {
-            dx = -dx;   
+        if (ballX<radius || ballX>windowX-radius){
+            dx=-dx;
         }
-        if (ballY < radius || ballY > windowY - radius) {
-            dy = -dy;   
+        if (ballY<radius || ballY>windowY-radius){
+            dy=-dy;   
         }
-
+        // Mise à jour de la position
+        ballX+=dx;
+        ballY+=dy;
         // Dessin
         BeginDrawing();
         ClearBackground(DARKGREEN);
-
         // 1. Le grand rectangle du terrain
         DrawRectangleLines(fieldMarginX, fieldMarginY, fieldWidth, fieldHeight, WHITE);
+        // ligne du rectangle, suelement por le contour
 
         // 2. LA LIGNE MÉDIANE (coupe le terrain en deux)
-        DrawLine(windowX / 2, fieldMarginY, windowX / 2, fieldMarginY + fieldHeight, WHITE);
+        DrawLine(windowX/2, fieldMarginY, windowX/2, fieldMarginY+fieldHeight,WHITE);
+        // winx divise 2 pour le mettre au mileu
 
         // 3. LE ROND CENTRAL (au milieu du terrain)
-        DrawCircleLines(windowX / 2, windowY / 2, 80, WHITE);
+        DrawCircleLines(windowX/2, windowY/2, 80, WHITE);// aussi le milieu (divise deux)
 
         // 4. But Gauche et But Droit
         DrawRectangleLines(fieldMarginX - goalWidth, goalY, goalWidth, goalHeight, WHITE);
@@ -63,3 +58,4 @@ int main() {
     CloseWindow();
     return 0;
 }
+
