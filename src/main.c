68b96df701aa42495e5483c7c7cb7b1eadb37793
ridouaneabakar-253 <ridouaneabakar@iut-dhaka.edu>
2026-08-01 20:07:25 +0600

    Connected game to main

--- src/main.c
+++ src/main.c
@@ -1,27 +1,37 @@
 #include "raylib.h"
+#include "game.h"
+
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "Game");
+    int windowX = 1350;
+    int windowY = 800;
+    InitWindow(windowX, windowY, "HABAH_BABAH");
 
     SetTargetFPS(60);
 
+    InitGame();
+
+    Texture2D fieldTexture = LoadTexture("field.png");
+
     while (!WindowShouldClose())
     {
-        // Update
-
+        UpdateGame();
 
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        // Draw
+        DrawTexture(fieldTexture, 0, 0, WHITE);
+
+        DrawGame(windowX, windowY);
 
         EndDrawing();
     }
+    // Unload before closing
+    UnloadTexture(fieldTexture);
+
+    CloseGame();
 
     CloseWindow();
 
